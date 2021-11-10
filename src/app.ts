b57446afc4a5c97ef4e4d7b1a07f0ffb5b0a051a
Rodrigo Vieira Baia <rodrigo@novasistemas rodrigo.baia@gmail.com>
2021-11-10 01:04:01 -0300

    organização

--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,10 @@ app.get("/github", (request,response) =>{
     response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENTE_ID}`);
 });
 
-
 app.get("/sign/callback", (request,response)=>{
     const {code} = request.query;
 
     return response.json(code);
 });
+
 app.listen(4000, ()=>console.log('Server listner port 4000'));
