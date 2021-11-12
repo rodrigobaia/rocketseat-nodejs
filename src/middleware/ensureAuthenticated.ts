125e6a7270ad9955d94791cc48cacb7c732dd721
Rodrigo Vieira Baia <rodrigo@novasistemas rodrigo.baia@gmail.com>
2021-11-12 02:37:07 -0300

    Estrutura de message

---
+++ src/middleware/ensureAuthenticated.ts
@@ -0,0 +1,31 @@
+import {Request, Response, NextFunction} from "express"
+import { verify} from "jsonwebtoken"
+
+interface IPayload{
+    sub: String   
+}
+
+
+export function ensureAuthenticated(request: Request, response: Response, next: NextFunction){
+    const authToken = request.headers.authorization;
+
+    if(!authToken){
+        return response.status(401).json({
+            erroCode: "token.invalid.",
+        });
+    }
+
+    const [,token] = authToken.split(" ")
+    try{
+        const { sub } = verify(token, process.env.JWT_SCECRET) as IPayload
+
+        request.user_id = sub
+
+        return next();
+    }
+    catch(err){
+        return response.status(401).json({erroCode: "token.expired"})
+    }
+
+    
+}
---
+++ src/services/CreateMessageService.ts
@@ -0,0 +1,21 @@
+import prismaCliente from "../prisma"
+
+
+
+class CreateMessageService{
+    async execute(text: string, user_id: string){
+        const message = await prismaCliente.message.create({
+            data:{
+                text,
+                user_id
+            },
+            include: {
+                user: true
+            }
+        })
+
+        return message;
+    }
+}
+
+export {CreateMessageService}
